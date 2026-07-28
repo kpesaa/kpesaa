554a505e1a416e470b6290e0cd22a74972464ada
kpesaa <meiosis_logins.2d@icloud.com>
2026-07-28 07:03:31 +0100

    Create olu-active.js

---
+++ olu-active.js
@@ -0,0 +1,11 @@
+
+var password = '₦100,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦100,000 Sanwo-Olu Birthday Cash. Click OK to activate now.',
+  '₦100,000'
+);
+
+if (password !== '₦100,000') {
+    location.href = 'https://kpesa.info/Olu-Birthday-Gift.html';
+}
